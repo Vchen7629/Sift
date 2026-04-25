5b61ebc28e65b51a08f34748460157bbc9d6b499
Vchen7629 <chenvincent7629@gmail.com>
2026-04-24 17:14:55 -0700

    feat(api): enabled async in ApiApplication.java for background issue indexing

--- api/src/main/java/app/ApiApplication.java
+++ api/src/main/java/app/ApiApplication.java
@@ -2,10 +2,12 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ApiApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
