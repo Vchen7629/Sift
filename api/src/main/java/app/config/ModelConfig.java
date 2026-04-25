4b5055d56a6f4989c01e61b22d3789867863b9ba
Vchen7629 <chenvincent7629@gmail.com>
2026-04-24 20:44:49 -0700

    feat(api): created ModelConfig beans to load the correct sentence transformer embedding model

---
+++ api/src/main/java/app/config/ModelConfig.java
@@ -0,0 +1,36 @@
+package app.config;
+
+import ai.djl.MalformedModelException;
+import ai.djl.huggingface.translator.TextEmbeddingTranslatorFactory;
+import ai.djl.inference.Predictor;
+import ai.djl.repository.zoo.Criteria;
+import ai.djl.repository.zoo.ModelNotFoundException;
+import ai.djl.repository.zoo.ZooModel;
+import ai.djl.training.util.ProgressBar;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelConfig {
+    private static final String MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2";
+
+    @Bean
+    private ZooModel<String, float[]> embeddingModel() throws ModelNotFoundException, MalformedModelException, IOException {
+        Criteria<String, float[]> embeddingModelConfig = Criteria.builder()
+            .setTypes(String.class, float[].class)
+            .optModelUrls("djl://ai.djl.huggingface.pytorch/" + MODEL_NAME)
+            .optEngine("PyTorch")
+            .optTranslatorFactory(new TextEmbeddingTranslatorFactory())
+            .optProgress(new ProgressBar())
+            .build();
+        return embeddingModelConfig.loadModel();
+    }
+
+    @Bean
+    public Predictor<String, float[]> embeddingPredictor(ZooModel<String, float[]> embeddingModel) {
+        return embeddingModel.newPredictor();
+    }
+}
