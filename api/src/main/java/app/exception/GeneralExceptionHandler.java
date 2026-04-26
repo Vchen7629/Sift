8b562a68bde105eaa81dba11d40f661b9437c8ba
Vchen7629 <chenvincent7629@gmail.com>
2026-04-26 12:17:45 -0700

    refactor(api); replaced IllegalArgumentException with ConstraintViolationException since im using spring boot validation annotations

--- api/src/main/java/app/exception/GeneralExceptionHandler.java
+++ api/src/main/java/app/exception/GeneralExceptionHandler.java
@@ -1,13 +1,22 @@
 package app.exception;
 
+import java.io.IOException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import jakarta.validation.ConstraintViolationException;
+
 @ControllerAdvice
 public class GeneralExceptionHandler {
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> illegalArgumentHander(IllegalArgumentException e) {
-        return ResponseEntity.status(400).body(e.getMessage());
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<String> IOException(IOException e) {
+        return ResponseEntity.status(500).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> constraintViolationException(IOException e) {
+        return ResponseEntity.status(500).body("internal server error");
     }
 }
--- api/src/main/java/app/exception/GithubExceptionHandler.java
+++ api/src/main/java/app/exception/GithubExceptionHandler.java
@@ -1,7 +1,5 @@
 package app.exception;
 
-import java.io.IOException;
-
 import org.kohsuke.github.GHFileNotFoundException;
 import org.kohsuke.github.HttpException;
 import org.springframework.http.ResponseEntity;
@@ -9,22 +7,17 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GithubExceptionHandler {
     @ExceptionHandler(GHFileNotFoundException.class)
-    public ResponseEntity<String> handleNotFound(GHFileNotFoundException e) {
+    public ResponseEntity<String> gHFileNotFoundException(GHFileNotFoundException e) {
         return ResponseEntity.status(404).body("Repository not found");
     }
 
     @ExceptionHandler(HttpException.class)
-    public ResponseEntity<String> handleHttpException(HttpException e) {
+    public ResponseEntity<String> httpException(HttpException e) {
         if (e.getResponseCode() == 403) {
             return ResponseEntity.status(403).body("GitHub rate limit exceeded");
         }
-        return ResponseEntity.status(500).body("Github API error");
-    }
-
-    @ExceptionHandler(IOException.class)
-    public ResponseEntity<String> handleIOException(IOException e) {
         return ResponseEntity.status(500).body("Github API error");
     }
 }
