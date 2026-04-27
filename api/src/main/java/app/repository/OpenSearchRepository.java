9f17d79085278f01269fe9464d544a8eba4df8e2
Vchen7629 <chenvincent7629@gmail.com>
2026-04-27 12:02:22 -0700

    feat(api): added hybrid search method in OpenSearchRepository.java

--- api/src/main/java/app/repository/OpenSearchRepository.java
+++ api/src/main/java/app/repository/OpenSearchRepository.java
@@ -4,17 +4,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.opensearch._types.aggregations.Aggregate;
 import org.opensearch.client.opensearch._types.aggregations.StringTermsBucket;
+import org.opensearch.client.opensearch._types.query_dsl.Query;
+import org.opensearch.client.opensearch._types.query_dsl.TextQueryType;
 import org.opensearch.client.opensearch.core.BulkResponse;
 import org.opensearch.client.opensearch.core.DeleteByQueryResponse;
+import org.opensearch.client.opensearch.core.SearchRequest;
 import org.opensearch.client.opensearch.core.SearchResponse;
 import org.opensearch.client.opensearch.core.bulk.BulkOperation;
+import org.opensearch.client.opensearch.search_pipeline.ScoreCombinationTechnique;
+import org.opensearch.client.opensearch.search_pipeline.ScoreNormalizationTechnique;
 import org.springframework.stereotype.Repository;
 import org.springframework.validation.annotation.Validated;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import ai.djl.translate.TranslateException;
 import app.service.TextEmbeddingService;
 import jakarta.annotation.PostConstruct;
 import jakarta.validation.constraints.NotBlank;
@@ -24,14 +33,21 @@ import jakarta.validation.constraints.NotEmpty;
 @Validated
 public class OpenSearchRepository {
     private final OpenSearchClient openSearchClient;
+    private final TextEmbeddingService textEmbeddingService;
+    private final static String indexName = "github-issues";
 
-    public OpenSearchRepository(OpenSearchClient openSearchClient) {
+    public OpenSearchRepository(
+        OpenSearchClient openSearchClient,
+        TextEmbeddingService textEmbeddingService
+    ) {
         this.openSearchClient = openSearchClient;
+        this.textEmbeddingService = textEmbeddingService;
     }
 
     @PostConstruct
     private void init() throws IOException {
-        createIndexIfNotExist("github-issues");
+        createIndexIfNotExist(indexName);
+        createSearchPipelineIfNotExist();
     }
 
     public void indexGithubIssue(@NotEmpty List<TextEmbeddingService.embeddingDocument> issueDocuments) throws IOException {
@@ -45,7 +61,7 @@ public class OpenSearchRepository {
         }
                 
         BulkResponse bulkRes = openSearchClient.bulk(r -> r
-            .index("github-issues")
+            .index(indexName)
             .operations(operations)
         );
 
@@ -63,29 +79,95 @@ public class OpenSearchRepository {
         }
     }
 
-    public boolean isRepoIndexed(String repoName) throws IOException{
-        SearchResponse<Void> searchRes = openSearchClient.search(r -> r
-            .index("github-issues")
-            .timeout("30s")
+    public void deleteTrackedRepo(@NotBlank String repoName) throws IOException {
+        DeleteByQueryResponse deleteRes =  openSearchClient.deleteByQuery(d -> d
+            .index(indexName)
             .query(q -> q
                 .term(t -> t
                     .field("repoName")
                     .value(v -> v.stringValue(repoName))
                 )
-            ), 
-            Void.class
+            )
         );
 
-        var total = searchRes.hits().total();
-
-        return total != null && total.value() > 0;
+        if (deleteRes.deleted() == 0) {
+            throw new NoSuchElementException("No repo found to delete");
+        }
     }
 
-    public List<String> listAllIndexedRepoNames() throws IOException {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static record IssueSearchResult(String url, String title, String body) {};
+
+    public List<IssueSearchResult> findRelevantIssues(
+        @NotBlank String repoUrl,
+        @NotBlank String searchQuery
+    ) throws TranslateException, IOException {
+        Query keywordQuery = Query.of(q -> q
+            .multiMatch(m -> m
+                .fields("title^1.1", "body")
+                .query(searchQuery)
+                .type(TextQueryType.BestFields)
+            )
+        );
+
+        float[] searchEmbedding = textEmbeddingService.embedText(searchQuery);
+        List<Float> searchVector = new ArrayList<>();
+        for (float f : searchEmbedding) searchVector.add(f); 
+
+        Query titleSemQuery = Query.of(q -> q
+            .knn(k -> k
+                .field("titleEmbedding")
+                .vector(searchVector)
+                .k(10)
+            )
+        );
+
+        Query bodySemQuery = Query.of(q -> q
+            .knn(k -> k
+                .field("bodyEmbedding")
+                .vector(searchVector)
+                .k(10)
+            )
+        );
+
+        Query hybridQuery = Query.of(q -> q
+            .hybrid(h -> h
+                .queries(keywordQuery, titleSemQuery, bodySemQuery)
+            )
+        );
+
+        var resultAmount = 10;
+        SearchRequest searchRequest = SearchRequest.of(s -> s
+            .index(indexName)
+            .query(q -> q
+                .bool(b -> b
+                    .filter(
+                        f -> f
+                        .term(t -> t
+                            .field("repoUrl")
+                            .value(v -> v.stringValue(repoUrl))
+                        )
+                    )
+                    .must(hybridQuery)
+                )
+            )
+            .size(resultAmount)
+            .timeout("30s")
+        );
+
+        SearchResponse<IssueSearchResult> searchRes = openSearchClient.search(searchRequest, IssueSearchResult.class);
+    
+        return searchRes.hits().hits().stream()
+            .map(hit -> hit.source())
+            .filter(Objects::nonNull)
+            .toList();
+    }
+
+    public List<String> findAllIndexedRepoNames() throws IOException {
         final int maxUniqueRepos = 1000;
 
         SearchResponse<Void> searchRes = openSearchClient.search(r -> r
-            .index("github-issues")
+            .index(indexName)
             .size(0) // need this so we dont return the actual document, use aggregations to return the strings instead
             .timeout("30s")
             .aggregations("repoNames", a -> a
@@ -107,21 +189,24 @@ public class OpenSearchRepository {
             .toList();
     }
 
-    public void deleteTrackedRepo(@NotBlank String repoName) throws IOException {
-        DeleteByQueryResponse deleteRes =  openSearchClient.deleteByQuery(d -> d
-            .index("github-issues")
+    public boolean isRepoIndexed(String repoName) throws IOException{
+        SearchResponse<Void> searchRes = openSearchClient.search(r -> r
+            .index(indexName)
+            .timeout("30s")
             .query(q -> q
                 .term(t -> t
                     .field("repoName")
                     .value(v -> v.stringValue(repoName))
                 )
-            )
+            ), 
+            Void.class
         );
 
-        if (deleteRes.deleted() == 0) {
-            throw new NoSuchElementException("No repo found to delete");
-        }
+        var total = searchRes.hits().total();
+
+        return total != null && total.value() > 0;
     }
+
 
     // dim number from: https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
     private static final Integer embeddingDim = 384;
@@ -154,6 +239,32 @@ public class OpenSearchRepository {
                             .engine("lucene")
                         )
                     ))
+                )
+            );
+        }
+    }
+
+    private void createSearchPipelineIfNotExist() throws IOException {
+        boolean exists = openSearchClient.searchPipeline()
+            .get(r -> r.id("hybrid-search-pipeline"))
+            .result()
+            .isEmpty();
+        
+        if (!exists) {
+            openSearchClient.searchPipeline().put(r -> r
+                .id("hybrid-search-pipeline")
+                .phaseResultsProcessors(p -> p
+                    .normalizationProcessor(n -> n
+                        .normalization(norm -> norm
+                            .technique(ScoreNormalizationTechnique.MinMax)
+                        )
+                        .combination(comb -> comb
+                            .technique(ScoreCombinationTechnique.ArithmeticMean)
+                            .parameters(params -> params
+                                .weights(List.of(0.3f, 0.35f, 0.35f)) // 30% BM25 35% titleEmb KNN 35% bodyEmb KNN
+                            )
+                        )
+                    )
                 )
             );
         }
