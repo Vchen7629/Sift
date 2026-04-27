f9b93aeb7a6f5fa963b2ec197378e8805f8f5b20
Vchen7629 <chenvincent7629@gmail.com>
2026-04-27 12:05:26 -0700

    feat(api): updated gitissuescontroller.java search endpoint with hybrid search method and guard against repos that aren't indexed in db

--- api/src/main/java/app/controller/GitIssuesController.java
+++ api/src/main/java/app/controller/GitIssuesController.java
@@ -1,5 +1,8 @@
 package app.controller;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ai.djl.translate.TranslateException;
+import app.repository.OpenSearchRepository;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 
@@ -14,12 +19,25 @@ import jakarta.validation.constraints.NotBlank;
 @RequestMapping("/issue")
 @Validated
 public class GitIssuesController {
+    private final OpenSearchRepository openSearchRepository;
 
-    private record searchIssueRequest(@NotBlank String query) {}
+    public GitIssuesController(OpenSearchRepository openSearchRepository) {
+        this.openSearchRepository = openSearchRepository;
+    }
+
+    private record searchIssueRequest(@NotBlank String repoUrl, @NotBlank String searchQuery) {}
     
     @PostMapping("/search")
-    public ResponseEntity<Void> searchIssue(@RequestBody @Valid searchIssueRequest query) {
+    public ResponseEntity<?> searchIssue(@RequestBody @Valid searchIssueRequest query) throws TranslateException, IOException {
+        if (!openSearchRepository.isRepoIndexed(query.repoUrl)) {
+            return ResponseEntity.status(404).body("Repository isn't indexed, no relevant issues");
+        }
 
-        return ResponseEntity.ok().build();
+        List<OpenSearchRepository.IssueSearchResult> issueResult =  openSearchRepository.findRelevantIssues(
+            query.repoUrl,
+            query.searchQuery
+        );
+
+        return ResponseEntity.ok().body(issueResult);
     }
 }
