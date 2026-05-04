6ee599915a84688bacdf04556f6f6a3f521e5a02
Vchen7629 <chenvincent7629@gmail.com>
2026-05-03 20:47:58 -0700

    test(index-repo-worker): created unit test for cleanIssueBody

---
+++ index-repo-worker/src/test/java/app/service/githubRepo/IssueServiceUT.java
@@ -0,0 +1,39 @@
+package app.service.githubRepo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.kohsuke.github.GitHub;
+import org.mockito.Mockito;
+
+public class IssueServiceUT {
+    private IssueService issueService;
+
+    @BeforeEach
+    void setup() {
+        issueService = new IssueService(Mockito.mock(GitHub.class));
+    }
+    
+    @Test
+    void cleanIssueBody_RemovesMarkdownText() throws IOException, URISyntaxException {
+        String unProcessedText = Files.readString(
+            Path.of(getClass().getClassLoader().getResource("service/issue_body.txt").toURI())
+        );
+        String processedText = Files.readString(
+            Path.of(getClass().getClassLoader().getResource("service/processed_body.txt").toURI())
+        );
+        String result = issueService.cleanIssueBody(unProcessedText);
+
+
+        assertEquals(result, processedText);
+        assertFalse(result.contains("```"));
+    }
+
+}
