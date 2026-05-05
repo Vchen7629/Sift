165ca7ea9080fb2cbdf141c737f6e6cab82ea489
Vchen7629 <chenvincent7629@gmail.com>
2026-05-04 21:17:32 -0700

    feat(api): added a schedule opensearch ping to keep jit warm and reduce query latency after a while of inactivity

---
+++ api/src/main/java/app/service/ConnectionWarmerService.java
@@ -0,0 +1,31 @@
+package app.service;
+
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
+
+@Service
+@Slf4j
+public class ConnectionWarmerService {
+    private final OpenSearchClient client;
+
+    public ConnectionWarmerService(OpenSearchClient client) {
+        this.client = client;
+    }
+
+    @Scheduled(fixedDelay = 300000) // 5 minutes
+    public void openSearch() {
+        try {
+            client.ping();
+            log.debug("pinged opensearch to keep connection alive");
+        } catch (Exception e) {
+            log.error("failed to ping the opensearch client to keep connection warm", 
+                kv("error", e.getMessage())
+            );
+        }
+    }
+}
