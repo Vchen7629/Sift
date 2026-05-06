ad7bff0f338cfd368e91fd62b1ab287e4392f0ed
Vchen7629 <chenvincent7629@gmail.com>
2026-05-05 21:24:14 -0700

    feat(tui): create status bar stub

---
+++ tui/internal/ui/components/status_bar.go
@@ -0,0 +1,19 @@
+package components
+
+import tea "charm.land/bubbletea/v2"
+
+type StatusBarModel struct {
+	height, width int
+}
+
+func (m StatusBarModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m StatusBarModel) Update(msg tea.Msg) tea.Cmd {
+	return nil
+}
+
+func (m StatusBarModel) View() string {
+	return "IF OUR LOVEEEEEEE IS TRAGEDY WHY ARE YOU MY REMEDYYYYYYYYY"
+}
