b9d148f2976488ad7c7537ff14b9cf2b18884007
Vchen7629 <chenvincent7629@gmail.com>
2026-05-06 16:39:34 -0700

    feat(tui): updated app.go to initialize user_repos view and accept the key presses

--- tui/internal/ui/app.go
+++ tui/internal/ui/app.go
@@ -5,48 +5,66 @@ import (
 	"charm.land/lipgloss/v2"
 
 	"tui/internal/ui/components"
-	"tui/internal/ui/styles"
+	"tui/internal/ui/context"
 	"tui/internal/ui/views"
 )
 
-type page int
-
-const (
-	authPage page = iota
-	queryPage
-	userReposPage
-)
-
 type model struct {
-	width, height int
-	currentPage   page
-	pages 		  map[page]tea.Model
+	ctx 		  *context.App
+	pages		  map[context.Page]tea.Model
 	statusBar 	  components.StatusBarModel
 }
 
 // constructor to initialize the pages map
 func New() model {
-	return model{
-		currentPage: authPage,
-		pages: map[page]tea.Model{
-			authPage: 	   views.AuthModel{},
-			queryPage:     views.QueryModel{},
-			userReposPage: views.UserRepoModel{},
+	ctx := context.NewApp()
+	return model{ 
+		ctx: ctx,
+		pages: map[context.Page]tea.Model{
+			context.AuthPage:	   views.AuthModel{Ctx: ctx},
+			context.QueryPage: 	   views.QueryModel{Ctx: ctx},
+			context.UserReposPage: &views.UserRepoModel{
+				Ctx: ctx,
+				RepoList: &components.UserRepoListModel{
+					Ctx: ctx,
+					FetchedRepos: []components.UserRepo{
+						{Name: "react", Status: "indexed", LastIndexed: "1746", TotalLibs: "42"},
+						{Name: "next.js", Status: "indexed", LastIndexed: "1745", TotalLibs: "18"},
+						{Name: "tailwindcss", Status: "pending", LastIndexed: "0", TotalLibs: "5"},
+						{Name: "react", Status: "indexed", LastIndexed: "1746", TotalLibs: "420"},
+						{Name: "next.js", Status: "indexed", LastIndexed: "1745", TotalLibs: "18"},
+						{Name: "tailwindcss", Status: "pending", LastIndexed: "0", TotalLibs: "5"},
+						{Name: "react", Status: "indexed", LastIndexed: "1746", TotalLibs: "42"},
+						{Name: "next.js", Status: "indexed", LastIndexed: "1745", TotalLibs: "18"},
+						{Name: "tailwindcss", Status: "pending", LastIndexed: "0", TotalLibs: "5"},
+						{Name: "react", Status: "indexed", LastIndexed: "1746", TotalLibs: "42"},
+						{Name: "next.js", Status: "indexed", LastIndexed: "1745", TotalLibs: "18"},
+						{Name: "tailwindcss", Status: "pending", LastIndexed: "0", TotalLibs: "5"},
+						{Name: "react", Status: "indexed", LastIndexed: "1746", TotalLibs: "42"},
+						{Name: "next.js", Status: "indexed", LastIndexed: "1745", TotalLibs: "18"},
+						{Name: "tailwindcss", Status: "pending", LastIndexed: "0", TotalLibs: "5"},
+					},
+				},
+			},
 		},
-		statusBar: components.StatusBarModel{},
+		statusBar:  components.StatusBarModel{Ctx: ctx},
 	}
 }
 
 func (m model) Init() tea.Cmd {
-	return m.pages[m.currentPage].Init()
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.width = msg.Width
-		m.height = msg.Height
-		m.statusBar.SetSize(msg.Width, 2)
+		m.ctx.Width = msg.Width
+		m.ctx.Height = msg.Height
+		m.ctx.ViewPortWidth = msg.Width - 55
+		m.ctx.ViewPortHeight = msg.Height - 3
+		m.statusBar.SetSize(msg.Width - 2, 1)
+
+		return m, nil
 
 	case tea.KeyPressMsg:
 		switch msg.String() {
@@ -55,22 +73,26 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	}
 
-	updated, cmd := m.pages[m.currentPage].Update(msg)
-	m.pages[m.currentPage] = updated
+	sbCmd := m.statusBar.Update(msg)
 
-	return m, cmd
+	updated, cmd := m.pages[m.ctx.CurrentPage].Update(msg)
+	m.pages[m.ctx.CurrentPage] = updated
+
+	return m, tea.Batch(sbCmd, cmd)
 }
 
 func (m model) View() tea.View {
-	pageContent := m.pages[m.currentPage].View()
-
-	main := lipgloss.NewStyle().
-		Width(m.width).
-		Height(m.height - 1).
-		Background(styles.Background).
-		Render(pageContent.Content)
-
+	pageContent := m.pages[m.ctx.CurrentPage].View()
 	statusBar := m.statusBar.View()
 
-	return tea.NewView(lipgloss.JoinVertical(lipgloss.Left, main, statusBar.Content))
+	content := lipgloss.JoinVertical(lipgloss.Left, pageContent.Content, statusBar.Content)
+
+	padding := lipgloss.NewStyle().
+		PaddingLeft(1).
+		PaddingRight(1).
+		Width(m.ctx.Width)
+
+	v := tea.NewView(padding.Render(content))
+	v.AltScreen = true
+	return v
 }
