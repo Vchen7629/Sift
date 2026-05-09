97e50b24993b0e0bb2ee7c3397c65c4d453deffd
Vchen7629 <chenvincent7629@gmail.com>
2026-05-08 20:09:37 -0700

    feat(tui): created method to fetch user github deta

---
+++ tui/internal/api/github.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	gh "github.com/cli/go-gh/v2/pkg/api"
+)
+
+type GithubClient struct {
+	rest *gh.RESTClient
+}
+
+func NewGithubClient() (*GithubClient, error) {
+	rest, err := gh.DefaultRESTClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return &GithubClient{rest: rest}, nil
+}
+
+// requires the gh cli tool installed and authenticated
+func (c *GithubClient) GithubUsername() (string, error) {
+	var response struct {
+		Login string `json:"login"`
+	}
+	err := c.rest.Get("user", &response)
+
+	return response.Login, err
+}
+
+type RepoApiRes struct {  
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	LastCommit  string `json:"pushed_at"`                                                                                                             
+}
+
+type RepoList struct {
+	Id				  			  int
+	Name, Description, LastCommit string
+}
+
+func (c *GithubClient) GithubUserRepositories() ([]RepoList, error){
+	var apiRes []RepoApiRes
+	err := c.rest.Get("user/repos?affiliation=owner&per_page=100", &apiRes)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]RepoList, 0, len(apiRes))
+	for i, repo := range apiRes {
+		res = append(res, RepoList{Id: i, Name: repo.Name, Description: repo.Description, LastCommit: repo.LastCommit})
+	}
+
+	return res, err
+}
