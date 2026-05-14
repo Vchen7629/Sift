1b16057484dbf40f809d3c39d9ccdd0adfb3ee5b
Vchen7629 <chenvincent7629@gmail.com>
2026-05-13 19:33:40 -0700

    fix(services): added actuator endpoint get to traces ignore list

--- services/index-repo-worker/src/main/java/app/config/ObservabilityConfig.java
+++ services/index-repo-worker/src/main/java/app/config/ObservabilityConfig.java
@@ -5,6 +5,7 @@ import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
 
 @Configuration
 public class ObservabilityConfig {
@@ -17,5 +18,11 @@ public class ObservabilityConfig {
     @Bean
     ObservationPredicate noScheduledTaskObservations() {
         return (name, context) -> !name.equals("tasks.scheduled.execution");
+    }
+
+    @Bean
+    ObservationPredicate noActuatorObservations() {
+        return (name, context) -> !(context instanceof ServerRequestObservationContext c
+            && c.getCarrier().getRequestURI().startsWith("/actuator"));
     }
 }
