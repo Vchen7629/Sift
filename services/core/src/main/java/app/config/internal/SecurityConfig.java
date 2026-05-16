c9688d8494e6a164503fd16b743d53d7146bd995
Vchen7629 <chenvincent7629@gmail.com>
2026-05-16 09:54:28 -0700

    feat(core): created bean for session based authentication

---
+++ services/core/src/main/java/app/config/internal/SecurityConfig.java
@@ -0,0 +1,27 @@
+package app.config.internal;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // disabled since requests come from tui
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/token").permitAll()
+                .anyRequest().authenticated());
+        
+        return http.build();
+    }
+}
